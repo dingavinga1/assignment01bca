bd0211ed415686aed256e2d67fb0ef219d141966
Abdullah Irfan <abdullahirfan@pk.folio3.com>
2023-09-16 04:52:12 -0700

    Minor fix

--- blockchain.go
+++ blockchain.go
@@ -5,19 +5,19 @@ import (
 	"fmt"
 )
 
-func CalculateHash(stringToHash string) string{
+func CalculateHash(stringToHash string) string{ // hashing function takes string as input and returns sha256 checksum
 	return fmt.Sprintf("%x", sha256.Sum256([]byte(stringToHash)));
 }
 
-type Block struct{
+type Block struct{ //structure that defines a single block in the chain
 	transaction string 
 	nonce int
-	prevPtr *Block
-	previousHash string
-	currentHash string 
+	prevPtr *Block // pointer to last block in the chain
+	previousHash string // hash of the previous block
+	currentHash string  // hash of transation+nonce+previousHash
 }
 
-func (blk *Block) Print() {
+func (blk *Block) Print() { // prints details for a single block in the chain
 	fmt.Printf("-------------------------------------------\n")
 	fmt.Printf("Transaction: %s\n", blk.transaction)
 	fmt.Printf("Nonce: %d\n", blk.nonce)
@@ -26,13 +26,14 @@ func (blk *Block) Print() {
 	fmt.Printf("-------------------------------------------\n")
 }
 
-func (blk *Block) Change(transaction string) {
+// Made a member function for Block instead of ChangeBlock function
+func (blk *Block) Change(transaction string) { // updates transaction within a single block to void integrity 
 	blk.transaction=transaction;
 	blk.currentHash=CalculateHash(fmt.Sprintf("%s%d%s", blk.transaction, blk.nonce, blk.previousHash))
 	
 }
 
-func NewBlock(transaction string, nonce int, prevPtr *Block, previousHash string) *Block{
+func NewBlock(transaction string, nonce int, prevPtr *Block, previousHash string) *Block{ // allocating a new Block in memory and returns its pointer
 	stream:=fmt.Sprintf("%s%d%s", transaction, nonce, previousHash);
 	hash:=CalculateHash(stream);
 
@@ -46,15 +47,16 @@ func NewBlock(transaction string, nonce int, prevPtr *Block, previousHash string
 	return blk
 }
 
-type Blockchain struct{
-	NewestBlock *Block
+type Blockchain struct{ // structure that defines a blockchain
+	NewestBlock *Block // pointer to the latest block
 }
 
-func (bc *Blockchain) AddBlock(transaction string, nonce int) *Block{
-	if bc.NewestBlock==nil{
+func (bc *Blockchain) AddBlock(transaction string, nonce int) *Block{ // adds a new block to the chain and returns a pointer to that block
+	if bc.NewestBlock==nil{ // check for adding first block
 		bc.NewestBlock=NewBlock(transaction, nonce, nil, "")
+		return bc.NewestBlock
 	}
-
+	// adding 
 	blk:=NewBlock(transaction, nonce, bc.NewestBlock, bc.NewestBlock.currentHash)
 	bc.NewestBlock=blk;
 
