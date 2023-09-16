68508bcced2dc8a6a6ad9b3626a8fa328024453b
Abdullah Irfan <abdullahirfan@pk.folio3.com>
2023-09-16 05:08:00 -0700

    v0.1.1 and comments

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module main
 
 go 1.21.1
 
-require github.com/dingavinga1/assignment01bca v0.1.0
+require github.com/dingavinga1/assignment01bca v0.1.1
--- main.go
+++ main.go
@@ -6,25 +6,34 @@ import (
 )
 
 func main(){
-	var bc assignment01bca.Blockchain;
-	bc.NewestBlock=nil
+	var bc assignment01bca.Blockchain; // creating a blockchain object
+	bc.NewestBlock=nil // initializing blockchain
 
+	// adding various transactions
 	bc.AddBlock("I am Aisha", 0)
 	bc.AddBlock("I am ok", 1)
 	bc.AddBlock("I am Abdullah", 2)
 	toBeChanged:=bc.AddBlock("I am ok", 3)
-	toBeChanged.Print()
 	bc.AddBlock("I am Huzaifa", 4)
 	bc.AddBlock("I am ok", 5)
 	bc.AddBlock("I am Usman", 6)
 	bc.AddBlock("I am ok", 7)
 
-	bc.ListBlocks()
+	bc.ListBlocks() //listing blockchain before change
 
-	toBeChanged.Change("I am not ok")
-	bc.ListBlocks()
-
+	// verifying blockchain before change
 	isOk:=bc.Verify()
+	if isOk==false {
+		fmt.Println("Blockchain has been compromised!")
+	} else{
+		fmt.Println("Blockchain is intact")
+	}
+
+	toBeChanged.Change("I am not ok") //changing a block
+	bc.ListBlocks() // listing blockchain after change
+
+	// verifying blockchain after change
+	isOk=bc.Verify()
 	if isOk==false {
 		fmt.Println("Blockchain has been compromised!")
 	} else{
