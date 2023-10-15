705dcff9b32221cc49acbbf5d24c4e23717dddb6
dingavinga1 <88616338+dingavinga1@users.noreply.github.com>
2023-10-15 06:44:45 -0700

    PoW demonstration+Competition

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module main
 
 go 1.21.1
 
-require github.com/dingavinga1/assignment01bca v0.1.1
+require github.com/dingavinga1/dvcoin v0.2.0 // indirect
--- main.go
+++ main.go
@@ -2,42 +2,94 @@ package main
 
 import (
 	"fmt"
-	"github.com/dingavinga1/assignment01bca"
+	"sync"
+	"time"
+
+	dvcoin "github.com/dingavinga1/dvcoin"
 )
 
-func main(){
-	var bc assignment01bca.Blockchain; // creating a blockchain object
-	bc.NewestBlock=nil // initializing blockchain
+func HoldCompetition(nodes []*dvcoin.Node, numNodes int){
+	var mu sync.Mutex //Mutex for thread sync
 
-	// adding various transactions
-	bc.AddBlock("I am Aisha", 0)
-	bc.AddBlock("I am ok", 1)
-	bc.AddBlock("I am Abdullah", 2)
-	toBeChanged:=bc.AddBlock("I am ok", 3)
-	bc.AddBlock("I am Huzaifa", 4)
-	bc.AddBlock("I am ok", 5)
-	bc.AddBlock("I am Usman", 6)
-	bc.AddBlock("I am ok", 7)
+	wg := new(sync.WaitGroup) //Wait group for joining
+ 
+	nope:=false //flag for winner
+	for i := 0; i < numNodes; i++ { //creating threads for each miner
+		wg.Add(1) //adding new thread to workgroup
+		
+		go func(id int) { //function for participation in the mining competition
+			defer wg.Done()
+			time.Sleep(1 * time.Second) //waiting for all threads initial sync
 
-	bc.ListBlocks() //listing blockchain before change
+			/* Mining */
+			blk := nodes[id].BuildBlock(nodes[id].ChooseTopTransactions()) 
+			nonce := nodes[id].Mine(blk)
 
-	// verifying blockchain before change
-	isOk:=bc.Verify()
-	if isOk==false {
-		fmt.Println("Blockchain has been compromised!")
-	} else{
-		fmt.Println("Blockchain is intact")
+			verified := nodes[id].Chain.VerifyNonce(blk, nonce) //system check for validating proposed nonce
+
+			if verified { 
+				mu.Lock() //Acquiring lock
+
+				if nope{ //Loser check
+					fmt.Printf("Sorry, Node#%d. You Lost!\n", id)
+					mu.Unlock()
+					return
+				} else{ //Winner
+					nope=true
+					nodes[id].Chain.AddBlock(blk, nonce) //Adding block to the chain
+					fmt.Printf("Congratulations, Node#%d! You won and your proposed block has been added.\n", id)
+					mu.Unlock()
+					return
+				}	
+			} else { //Check for invalid nonce
+				fmt.Printf("Node#%d, you're a cheater.\n", id)
+				return
+			}
+		}(i)
 	}
 
-	toBeChanged.Change("I am not ok") //changing a block
-	bc.ListBlocks() // listing blockchain after change
+	wg.Wait() //waiting for all threads to finish execution
+}	
 
-	// verifying blockchain after change
-	isOk=bc.Verify()
-	if isOk==false {
-		fmt.Println("Blockchain has been compromised!")
-	} else{
-		fmt.Println("Blockchain is intact")
+func main() {
+	var bc dvcoin.Blockchain //blockchain
+	bc.Difficulty = 2 //setting difficulty
+
+	numNodes := 3 //number of total nodes holding copies of the blockchain
+
+	var nodes []*dvcoin.Node
+	for i := 0; i < numNodes; i++ { //creating 3 nodes and holding copy of chain
+		nodes = append(nodes, &dvcoin.Node{})
+		nodes[i].Chain = &bc
 	}
+
+	/* Providing 6 initial transactions in the transaction pool */
+	bc.AddTransaction("Abdullah", "Aisha", 2)
+	bc.AddTransaction("Huzaifa", "Aisha", 3)
+	bc.AddTransaction("Aisha", "Huzaifa", 1)
+	bc.AddTransaction("Aisha", "Abdullah", 4)
+	bc.AddTransaction("Usman", "Abdullah", 8)
+	bc.AddTransaction("Abdullah", "Usman", 5)
+
+	fmt.Println("\t\t\t========================================")
+	fmt.Println("\t\t\t           Before competition")
+	fmt.Println("\t\t\t========================================")
+	bc.Print()
+
+	fmt.Println("\n\n\n\n\t\t\t========================================")
+	fmt.Println("\t\t\t         After 1st competition")
+	fmt.Println("\t\t\t========================================")
+	HoldCompetition(nodes, numNodes);
+	bc.Print()
+
+	/* Providing 2 more transactions in the transaction pool */
+	bc.AddTransaction("Usman", "Aisha", 10)
+	bc.AddTransaction("Huzaifa", "Usman", 8)
+
+	fmt.Println("\n\n\n\n\t\t\t========================================")
+	fmt.Println("\t\t\t        After final competition")
+	fmt.Println("\t\t\t========================================")
+	HoldCompetition(nodes, numNodes);
+	bc.Print()
 
 }
